838a675a8d7f9ac602e72c658bcb7afdb348c51c
Devdat Kumar <Devdat_Kumar@Outlook.com>
2024-10-04 11:38:09 +0500

    error check for connectionstring, pool connection with ssl for production mode, and logger for develper mode.

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,7 +1,27 @@
 import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
+import * as schema from "./schema";
 
-const connectionString = process.env.DATABASE_URL!;
+const connectionString = process.env.DATABASE_URL;
 
-const client = postgres(connectionString);
-export const db = drizzle(client);
+if (!connectionString) {
+  throw new Error("DATABASE_URL is not set in the environment variables");
+}
+
+const client = postgres(connectionString, {
+  max: 1,
+  prepare: false,
+  ssl:
+    process.env.NODE_ENV === "production"
+      ? { rejectUnauthorized: true }
+      : false,
+});
+
+export const db = drizzle(client, {
+  schema,
+  logger: process.env.NODE_ENV !== "production",
+});
+
+export const closeDbConnection = () => {
+  client.end();
+};
